41e614a7c45a3cfa7f52e52f6213560a8d3a992b
Niels van Dijk <niels.vandijk@surf.nl>
2021-12-13 12:31:12 +0100

    More updates

--- mk_docs.py
+++ mk_docs.py
@@ -158,8 +158,8 @@ def mkResources(root_dir, web_root):
 root_dir = os.path.expanduser('~') + "/"
 tempdir = root_dir + "ssp_tmp/"
 
-runner_path = '/home/runner/work/docs.simplesamlphp.org/docs.simplesamlphp.org'
-#os.popen('pwd $GITHUB_WORKSPACE').read()
+#Runner path eequals $GITHUB_WORKSPACE
+runner_path = '/home/runner/work/docs.simplesamlphp.org/docs.simplesamlphp.org/'
 
 repo_root_dir = "simplesamlphp/"
 repo_docs_dir = "docs/"
