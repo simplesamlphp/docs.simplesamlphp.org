97a065df8c1505b4e939901d6a2337c05eeb33e0
Niels van Dijk <niels.vandijk@surf.nl>
2021-12-15 15:52:52 +0100

    Fixing contrib modules index page

--- mk_docs.py
+++ mk_docs.py
@@ -163,7 +163,7 @@ def mkcontribmodsindex(contrib_mods, module_index_file, contrib_mods_files):
     for module in contrib_mods:
       module_index += " * ["+ module["name"] + "](" + module["html_url"] + ") \n" + module["description"] + "\n"
       
-      for page in pages[module["name"]:
+      for page in pages[module["name"]]:
         module_index += "   * ["+ page + "]\n"
           
     print(module_index)  
