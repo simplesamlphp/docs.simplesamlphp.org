7b0045bd308416f680d81fd79252539daae09130
Thijs Kinkhorst <thijs@kinkhorst.com>
2022-03-15 15:23:25 +0100

    Support fenced code blocks and highlighting

--- mk_docs.py
+++ mk_docs.py
@@ -8,7 +8,7 @@ import json
 from bs4 import BeautifulSoup
 
 # Set up the markdown converter
-md_ext = ['toc']
+md_ext = ['toc', 'fenced_code', 'codehilite']
 md_ext_confs = {
 }
 pymd = markdown.Markdown(extensions=md_ext,extension_configs=md_ext_confs, tab_length=2)
