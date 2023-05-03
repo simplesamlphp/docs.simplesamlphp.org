67bd1d75008fff4314b3f5265c27c5a706950d60
Tim van Dijen <tvdijen@gmail.com>
2023-05-03 10:40:34 +0200

    Fix for nested fences
    Python markdown only supports fenced code blocks on document-level.
    The superfences-extension should be able to support them

--- mk_docs.py
+++ mk_docs.py
@@ -8,10 +8,9 @@ import json
 from bs4 import BeautifulSoup
 
 # Set up the markdown converter
-md_ext = ['toc', 'fenced_code', 'codehilite', 'def_list']
-md_ext_confs = {
-}
-pymd = markdown.Markdown(extensions=md_ext,extension_configs=md_ext_confs, tab_length=2)
+md_ext = ['toc', 'fenced_code', 'codehilite', 'def_list', 'pymdownx.superfences']
+
+pymd = markdown.Markdown(extensions=md_ext, tab_length=2)
 
 # Convert md file to html file
 # - make sure links remain working
